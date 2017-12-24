645cd809244352ead7ce2ebc1700cb0941c74d4e
isaac219 <isaac219@gmail.com>
2017-12-23 21:54:32 -0500

    implemented threading to improve speed and practice python, and cleaned up code

--- dailyfeed.py
+++ dailyfeed.py
@@ -1,57 +1,63 @@
 #!/usr/bin/env python
-import praw, sys, datetime
+import praw, sys, datetime, time
+from threading import Thread
 
 if len(sys.argv) < 3:
     sys.exit("Syntax Error - Usage: %s recipient (subreddit)" % sys.argv[0])
 
 #initialize reddit instance
 reddit = praw.Reddit(client_id='',
-                     client_secret='',
-                     user_agent='',
-                     password='',
-                     username='')
+                    client_secret='',
+                    user_agent='',
+                    password='',
+                    username='')
 
-#get subreddits from CL arguments
-def get_subreddits():
+#get title and link from top daily post of each subreddit
+def get_post(sub, result, index):
+    try:
+        subreddit = reddit.subreddit(sub)
+        for submission in subreddit.top('day'):
+            subreddit_name = "**r/" + str(submission.subreddit) + "**"
+            title = str(submission.title)
+            link = str(submission.shortlink)
+            result[index] = (subreddit_name +"\n\n" + title +": " + link + "\n\n")
+            return True
+    except:
+        print("Error encountered for " + sub)
+        subreddit_name = "**r/" + str(subreddit) + "**" + "\n\n"
+        result[index] = subreddit_name +  "Error encountered for r/" + str(subreddit) + "\n\n"
+
+#create threads for retrieving each subreddit post and send the pm
+def thread_and_pm():
     sub_list = []
+
     for i in range (2,len(sys.argv)):
             sub_list.append(sys.argv[i])
-    return sub_list
 
-#map submissions into a dictionary with subreddit, title, and link
-def map_submissions(sub_list):
     global reddit
-    dictionary = {}
-    for sub in sub_list:
-        subreddit = reddit.subreddit(sub)
-        count = 0
-        for submission in subreddit.top("day"):       
-            title_and_link = [str(submission.title),str(submission.shortlink)]
-            dictionary.update({str(submission.subreddit): title_and_link})
-            count +=1
-            if count==1:
-                break
-    return dictionary
 
-#build and send the message to redditor
-def message(dictionary):
-    global reddit
-    body = ""
-    for key, value in dictionary.iteritems():
-        title = value[0]
-        link = value[1]
-        subreddit_name = "**r/" + str(key) + "**"
-        body+=(subreddit_name +"\n\n" + title +": " + link + "\n\n")
-    pm_subject = "Feed - " + str(datetime.date.today())
+    subject= "Feed - " + str(datetime.date.today())
     recipient = sys.argv[1]
-    reddit.redditor(recipient).message(pm_subject, body)
+    body = ""
 
-def main():
-    dictionary = map_submissions(get_subreddits())
-    message(dictionary)
+    threads = [None] * len(sub_list)
+    results = [None] * len(sub_list)
+
+    for i in range(len(sub_list)):
+        threads[i] = Thread(target=get_post,args=(sub_list[i], results, i))
+        threads[i].start()
+
+    for i in range(len(threads)):
+        threads[i].join()
+
+    for r in results:
+        if(r != None): 
+            body += r
+
+    reddit.redditor(recipient).message(subject, body)
 
 if __name__ == "__main__":
-    try:
-        main();
-    except:
-        reddit.redditor(sys.argv[1]).message("Daily Feed Error", "Daily feed bot has encountered an error");
+    start = time.time()
+    thread_and_pm()
+    end = time.time()
+    print(end - start)
